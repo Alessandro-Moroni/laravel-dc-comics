6f07a6b61b5c17e40be33e8509242c3b6b8489ef
Alessandro-Moroni <alemoro98@gmail.com>
2024-05-16 16:31:03 +0200

    [done]seeder

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+use App\Functions\Helper;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = config('products');
+
+        foreach($products as $product){
+            $new_comic = new Comic();
+            $new_comic->title = $product['title'];
+            $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
+            $new_comic->description = $product['description'];
+            $new_comic->thumb = $product['thumb'];
+            $new_comic->price = $product['price'];
+            $new_comic->series = $product['series'];
+            $new_comic->sale_date = $product['sale_date'];
+            $new_comic->type = $product['type'];
+            $new_comic->artists = json_encode($product['artists']);
+            $new_comic->writers = json_encode($product['writers']);
+
+            $new_comic->save();
+
+        }
+    }
+}
